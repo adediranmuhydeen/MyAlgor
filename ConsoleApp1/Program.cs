e237a5fbcff1f86648f3abdd23c225ae2b53e987
Muhydeen Adebayo <adediranma@examroom.ai>
2023-11-29 15:54:00 +0100

    Two Sum

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,9 @@ Console.WriteLine(TimeSpan.Parse(time));
 TimeSpan span = new TimeSpan(19, 0, 0);
 
 Console.WriteLine(DateTime.Parse(TimeOnly.FromTimeSpan(span).ToString()).ToString("t"));
-int[] arr = {2, 7, 11, 15};
-var result = LeetCode.TwoSum(arr, 9);
-foreach (int i in arr)
+int[] arr = {3,2,4};
+var result = LeetCode.TwoSum(arr, 6);
+foreach (int i in result)
 {
     Console.WriteLine(i);
 }
