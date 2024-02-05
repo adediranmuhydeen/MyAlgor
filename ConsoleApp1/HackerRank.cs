57227ab938b63740f3d92740e54e0f2970699277
Muhydeen Adebayo <adediranma@examroom.ai>
2024-02-05 22:00:14 +0100

    gradingStudents

--- ConsoleApp1/HackerRank.cs
+++ ConsoleApp1/HackerRank.cs
@@ -1,5 +1,7 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 using System.Reflection.Metadata;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -65,9 +67,42 @@ namespace ConsoleApp1
             double positive = plus / arr.Count;
             double negative = minus / arr.Count;
             double nutral = zero / arr.Count;
-            Console.WriteLine(positive.ToString("D4"));
-            Console.WriteLine(negative.ToString("D7"));
-            Console.WriteLine(nutral.ToString("D7"));
+            Console.WriteLine(positive.ToString());
+            Console.WriteLine(negative.ToString());
+            Console.WriteLine(nutral.ToString());
         }
+
+        /// <summary>
+        /// HackerLand University has the following grading policy: Every student receives a  in the inclusive range from 0 to 100. Any grade less than 40 is a failing grade. 
+        /// Sam is a professor at the university and likes to round each student's  according to these rules:the difference between the grade and the next multiple of 5 is less than 3,
+        /// round grade up to the next multiple of 5.If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+        /// </summary>
+        /// <param name="grades"></param>
+        /// <returns></returns>
+
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < grades.Count; i++)
+            {
+                var byOne = grades[i]+1;
+                var byTwo = grades[i] + 2;
+                if (byOne % 5 == 0 && grades[i] > 37)
+                {
+                    result.Add(byOne);
+                }
+                else if (byTwo % 5 == 0 && grades[i] > 37)
+                {
+                    result.Add(byTwo);
+                }
+                else
+                {
+                    result.Add(grades[i]);
+                }
+            }
+
+            return result;
+        }
+
     }
 }
